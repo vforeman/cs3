ee733fdcd22c3bce8e6b2572a656af78a441e684
V <V@As-iMac-3.local>
2014-04-23 09:01:52 -0400

    put code into a single header

--- RoomStruct.h
+++ RoomStruct.h
@@ -11,8 +11,20 @@ struct Room{
 	void readfile(ifstream & file);
 	Room * left_rm;
 	Room * right_rm;
+	Room();
+	~Room();
 };	
+Room::Room(){
 
+}
+Room::~Room(){
+	if(left_rm != NULL){
+		delete left_rm;
+	}
+	if(right_rm != NULL){
+		delete right_rm;
+	}
+}
 void Room::print(){
 	cout<<"room_number: "<<rn<<endl;
 	cout<<"storyline: "<<storyline<<endl;
@@ -35,6 +47,121 @@ void Room::readfile(ifstream & file){
 	getline(file,str_no_item);
 	file>>life_wo_item;
 	file>>life_regardless;
+}
+
+struct player
+{
+	int life;
+	int inventory[7];
+	int roomnum;
+	ifstream file;
+	Room * root;
+	Room * curRoom;
+	Room * rooms;
+	player();
+	~player();
+	void heapify(Room * parent, Room * child);
+	void buildheap();
+	void startGame();
+	void getInput();
+	void setUp();
+	void RoomPlay(Room * curr);
+
+};
+player::player(){
+	rooms = new Room[41];
+	file.open("info.txt");
+	for(int r = 0; file.good(); ++r){
+		rooms[r].readfile(file);
+	}
+	curRoom = root;
+	buildheap();
+	setUp();
+}
+player::~player(){
+	delete root;
+	delete[] rooms;
+}
+void player::heapify(Room * parent, Room * child ){
+	if(child->rn < parent->rn){
+		if(parent->left_rm == NULL){
+			parent->left_rm = child;
+		}else{
+			heapify(parent->left_rm, child);
+		}
+	}else{
+		if(parent->right_rm == NULL){
+			parent->right_rm = child;
+		}else{
+			heapify(parent->right_rm, child);
+		}
+	}
+}
+void player::buildheap(){
+	root = &rooms[0];
+	for(int r = 1; r< 41; ++r){
+		heapify(root,&rooms[r]);
+	}
+}
+
+void player::setUp()
+{
+ 	life = 0;
+ 	for(int x=0; x<=7; ++x)
+        inventory[x]=0;
+ 	roomnum = 100;
+}
+void player::startGame()
+{
+	RoomPlay(root);
+}
+void player::RoomPlay(Room * curr)
+{	
+	curRoom = curr;
+ 	int take;
+ 	char choice;
+ 	cout<<curRoom->storyline<<endl;
+ 	cout<<curRoom->room_item <<endl;
+ 	/*-------------PICKUP ITEM--------------*/
+ 	if (curRoom->room_item != 0){
+        cout <<"Take it (0) or Nah (1)\n";
+        cin >>take;
+        if (take == 0)
+            inventory[curRoom->room_item] = 1;
+        cout <<"Item: " <<inventory[curRoom->room_item] <<endl;
+ 	}
+ 	if (curRoom->required_item != 0){
+        if (inventory[curRoom->required_item] == 1){
+                 cout <<curRoom->str_had_item;
+                 life = curRoom->life_w_item;
+        }else{
+            cout <<curRoom->str_no_item;
+            life = curRoom->life_wo_item;
+        }
+ 	}else{
+        life = curRoom->life_regardless;
+        cout <<"Current life status: " <<life <<endl;
+ 	}
+ 	/*-------------PICK NEXT ROOM0--------------*/
+ 	if (life == 0){
+        cout<<"LEFT or RIGHT"<<endl;
+        cin>>choice;
+        if ((curRoom->rn == 1) || (curRoom->rn == 6)
+            || (curRoom->rn == 170) || (curRoom->rn == 190)
+            || (curRoom->rn == 290) || (curRoom->rn ==340)){
+            cout <<"You've survived the cave! Whew. Onward to save the lady!\n";
+            //Add 1 to # of wins
+            //Exit game
+        }else if ((choice == 'l') || (choice == 'L')){
+        	RoomPlay(curRoom->left_rm);
+        }else{
+	        RoomPlay(curRoom->right_rm);
+        }
+ 	}else{
+        cout <<"GAME OVER\n";
+        //Add 1 to # of deaths
+        //Exit game
+	}
 }
 
 #endif
--- app.cpp
+++ app.cpp
@@ -6,125 +6,121 @@
 using namespace std;
 
 
-void buildheap();
-void heapify(Room * parent, Room * child);
-Room * root;
-Room rooms[41];
+// void buildheap();
+// void heapify(Room * parent, Room * child);
+// Room * root;
+// Room rooms[41];
 
-struct player
-{
- int life;
- int inventory[7];
- int roomnum;
-};
-void startGame(player &user);
-void getInput();
-void setUp(player &person);
-void RoomPlay(Room &curRoom, player &p);
+
+// void startGame(player &user);
+// void getInput();
+// void setUp(player &person);
+// void RoomPlay(Room &curRoom, player &p);
 
 int main(){
+	player p;
+	p.startGame();
+	// ifstream file;
+	// file.open("info.txt");
+	// if(file.is_open()){
+	// 	cout<<"file opened\n";
+	// }
+	// else{
+	// 	cout<<"couldn't open file\n";
+	// 	return 0;
+	// }
+	// for(int r = 0; file.good(); ++r){
+	// 	rooms[r].readfile(file);
+	// }
+	// buildheap();
 
-	ifstream file;
-	file.open("info.txt");
-	if(file.is_open()){
-		cout<<"file opened\n";
-	}
-	else{
-		cout<<"couldn't open file\n";
-		return 0;
-	}
-	for(int r = 0; file.good(); ++r){
-		rooms[r].readfile(file);
-	}
-	buildheap();
-
-	//Initialize a player
- 	player p1;
-	//Set up the game
- 	setUp(p1);
-	//Play the game
- 	startGame(p1);
+	// //Initialize a player
+ // 	player p1;
+	// //Set up the game
+ // 	setUp(p1);
+	// //Play the game
+ // 	startGame(p1);
 
 	return 0;
 }
 
-void heapify(Room * parent, Room * child ){
-	if(child->rn < parent->rn){
-		if(parent->left_rm == NULL){
-			parent->left_rm = child;
-		}else{
-			heapify(parent->left_rm, child);
-		}
-	}else{
-		if(parent->right_rm == NULL){
-			parent->right_rm = child;
-		}else{
-			heapify(parent->right_rm, child);
-		}
-	}
-}
-void buildheap(){
-	root = &rooms[0];
-	for(int r = 1; r< 41; ++r){
-		heapify(root,&rooms[r]);
-	}
-}
+// void heapify(Room * parent, Room * child ){
+// 	if(child->rn < parent->rn){
+// 		if(parent->left_rm == NULL){
+// 			parent->left_rm = child;
+// 		}else{
+// 			heapify(parent->left_rm, child);
+// 		}
+// 	}else{
+// 		if(parent->right_rm == NULL){
+// 			parent->right_rm = child;
+// 		}else{
+// 			heapify(parent->right_rm, child);
+// 		}
+// 	}
+// }
+// void buildheap(){
+// 	root = &rooms[0];
+// 	for(int r = 1; r< 41; ++r){
+// 		heapify(root,&rooms[r]);
+// 	}
+// }
 
-void setUp(player &person)
-{
- 	person.life = 0;
- 	for(int x=0; x<=7; ++x)
-        person.inventory[x]=0;
- 	person.roomnum = 100;
-}
-void startGame(player &user)
-{
-	RoomPlay(*root, user);
-}
-void RoomPlay(Room &curRoom, player &p)
-{
- 	int take;
- 	char choice;
- 	cout<<curRoom.storyline<<endl;
- 	cout<<curRoom.room_item <<endl;
- 	/*-------------PICKUP ITEM--------------*/
- 	if (curRoom.room_item != 0){
-        cout <<"Take it (0) or Nah (1)\n";
-        cin >>take;
-        if (take == 0)
-            p.inventory[curRoom.room_item] = 1;
-        cout <<"Item: " <<p.inventory[curRoom.room_item] <<endl;
- 	}
- 	if (curRoom.required_item != 0){
-        if (p.inventory[curRoom.required_item] == 1){
-                 cout <<curRoom.str_had_item;
-                 p.life = curRoom.life_w_item;
-        }else{
-            cout <<curRoom.str_no_item;
-            p.life = curRoom.life_wo_item;
-        }
- 	}else{
-        p.life = curRoom.life_regardless;
-        cout <<"Current life status: " <<p.life <<endl;
- 	}
- 	/*-------------PICK NEXT ROOM--------------*/
- 	if (p.life == 0){
-        cout<<"LEFT or RIGHT"<<endl;
-        cin>>choice;
-        if ((curRoom.rn == 1) || (curRoom.rn == 6)
-            || (curRoom.rn == 170) || (curRoom.rn == 190)
-            || (curRoom.rn == 290) || (curRoom.rn ==340)){
-            cout <<"You've survived the cave! Whew. Onward to save the lady!\n";
-            //Add 1 to # of wins
-            //Exit game
-        }else if ((choice == 'l') || (choice == 'L')){
-        	RoomPlay(*curRoom.left_rm, p);
-        }else{
-	        RoomPlay(*curRoom.right_rm, p);
-        }
- 	}else{
-        cout <<"GAME OVER\n";
-        //Add 1 to # of deaths
-        //Exit game
-	}
-}
+// void setUp(player &person)
+// {
+//  	person.life = 0;
+//  	for(int x=0; x<=7; ++x)
+//         person.inventory[x]=0;
+//  	person.roomnum = 100;
+// }
+// void startGame(player &user)
+// {
+// 	RoomPlay(*root, user);
+// }
+// void RoomPlay(Room &curRoom, player &p)
+// {
+//  	int take;
+//  	char choice;
+//  	cout<<curRoom.storyline<<endl;
+//  	cout<<curRoom.room_item <<endl;
+//  	/*-------------PICKUP ITEM--------------*/
+//  	if (curRoom.room_item != 0){
+//         cout <<"Take it (0) or Nah (1)\n";
+//         cin >>take;
+//         if (take == 0)
+//             p.inventory[curRoom.room_item] = 1;
+//         cout <<"Item: " <<p.inventory[curRoom.room_item] <<endl;
+//  	}
+//  	if (curRoom.required_item != 0){
+//         if (p.inventory[curRoom.required_item] == 1){
+//                  cout <<curRoom.str_had_item;
+//                  p.life = curRoom.life_w_item;
+//         }else{
+//             cout <<curRoom.str_no_item;
+//             p.life = curRoom.life_wo_item;
+//         }
+//  	}else{
+//         p.life = curRoom.life_regardless;
+//         cout <<"Current life status: " <<p.life <<endl;
+//  	}
+//  	/*-------------PICK NEXT ROOM0--------------*/
+//  	if (p.life == 0){
+//         cout<<"LEFT or RIGHT"<<endl;
+//         cin>>choice;
+//         if ((curRoom.rn == 1) || (curRoom.rn == 6)
+//             || (curRoom.rn == 170) || (curRoom.rn == 190)
+//             || (curRoom.rn == 290) || (curRoom.rn ==340)){
+//             cout <<"You've survived the cave! Whew. Onward to save the lady!\n";
+//             //Add 1 to # of wins
+//             //Exit game
+//         }else if ((choice == 'l') || (choice == 'L')){
+//         	RoomPlay(*curRoom.left_rm, p);
+//         }else{
+// 	        RoomPlay(*curRoom.right_rm, p);
+//         }
+//  	}else{
+//         cout <<"GAME OVER\n";
+//         //Add 1 to # of deaths
+//         //Exit game
+// 	}
+// }
